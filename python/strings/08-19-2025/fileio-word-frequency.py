8e9c13babdd71bc616958501ce8a8a527b045f19
Abhinav K <abhinav14kr@zohomail.com>
2025-08-19 20:03:30 -0700

    reading files and aggregating

---
+++ python/strings/08-19-2025/fileio-word-frequency.py
@@ -0,0 +1,29 @@
+
+word_count = {}
+
+all_words = []
+
+with open('input.txt', 'r') as f:
+    for line in f: 
+        line = line.lower()
+        words = line.split()
+        all_words.extend(words)
+        print(words) 
+
+for word in all_words: 
+    if word in word_count: 
+        word_count[word] += 1
+    else: 
+        word_count[word] = 1
+
+print(word_count)
+
+
+top_5 = []
+
+for word, count in word_count.items():
+    top_5.append((word, count))
+
+top_5 = sorted(top_5, key = lambda x:x[1], reverse = True)[:5]
+
+print(top_5)
