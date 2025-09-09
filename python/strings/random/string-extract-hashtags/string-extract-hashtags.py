8506b35145e7f847de307bb02595ad1b48eb3ecd
Abhinav K <abhinav14kr@zohomail.com>
2025-09-08 23:26:14 -0700

    extracting hastags

---
+++ python/strings/random/string-extract-hashtags/string-extract-hashtags.py
@@ -0,0 +1,25 @@
+import re
+
+def extract_hashtags(text):
+    #  hashtags using regex
+    pattern = r'#\w+'
+    hashtags = re.findall(pattern, text)
+
+    # Removing duplicates and sorting
+    unique_hashtags = sorted(set(hashtags))
+
+    # Save to file
+    with open('hashtags.txt', 'w') as f:
+        for tag in unique_hashtags:
+            f.write(tag + '\n')
+
+    return unique_hashtags
+
+# example
+text = """
+Loving the new features in #Python! 
+Working on #DataEngineering projects. 
+#AI and #MachineLearning are the future. #Python
+"""
+
+print(extract_hashtags(text))
