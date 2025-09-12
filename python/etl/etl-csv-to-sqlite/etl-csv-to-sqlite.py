23947aa93f75b9bdd771af68386d346e6395e7fd
Abhinav K <abhinav14kr@zohomail.com>
2025-09-12 00:21:03 -0700

    etl day1

---
+++ python/etl/etl-csv-to-sqlite/etl-csv-to-sqlite.py
@@ -0,0 +1,16 @@
+import pandas as pd, sqlite3
+
+df = pd.read_csv("transactions.csv")
+print(df.head())
+
+clean_df = df.dropna(subset=["Customer"])
+print(clean_df.head())
+
+clean_df["Amount"] = pd.to_numeric(clean_df["Amount"], errors="coerce")
+normalized_dates = pd.to_datetime(clean_df["Date"], errors="coerce").dt.strftime("%Y-%m-%d")
+clean_df["Date"] = normalized_dates
+print(clean_df.head())  
+
+
+with sqlite3.connect("etl.db") as conn:
+    clean_df.to_sql("Transactions", conn, if_exists="replace", index=False)
