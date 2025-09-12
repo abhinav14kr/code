a86ad602d654e19898678569566391349a4f9db8
Abhinav K <abhinav14kr@zohomail.com>
2025-09-12 00:28:29 -0700

    starting with etl code

--- python/etl/etl-csv-to-sqlite/etl-csv-to-sqlite.py
+++ python/etl/etl-csv-to-sqlite/etl-csv-to-sqlite.py
@@ -7,7 +7,7 @@ clean_df = df.dropna(subset=["Customer"])
 print(clean_df.head())
 
 clean_df["Amount"] = pd.to_numeric(clean_df["Amount"], errors="coerce")
-normalized_dates = pd.to_datetime(clean_df["Date"], errors="coerce").dt.strftime("%Y-%m-%d")
+normalized_dates = pd.to_datetime(clean_df["Date"], errors="coerce", dayfirst=True).dt.strftime("%Y-%m-%d")
 clean_df["Date"] = normalized_dates
 print(clean_df.head())  
 
